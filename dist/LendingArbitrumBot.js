238dd97103985f921bb9f83e38bdd40099483661
phil <phil.softwaredev@protonmail.com>
2024-04-12 21:09:33 +0200

    increase $ filter

--- dist/LendingArbitrumBot.js
+++ dist/LendingArbitrumBot.js
@@ -1,12 +1,12 @@
-import { getWeb3WsProvider } from "./utils/helperFunctions/Web3.js";
-import { getCurrentBlockNumber, getPastEvents } from "./utils/web3Calls/generic.js";
-import { telegramBotMain } from "./utils/telegram/TelegramBot.js";
-import { EventEmitter } from "events";
-import { handleLiveEvents, manageMarket, watchingForNewMarketOpenings } from "./utils/Oragnizer.js";
-import { ADDRESS_crvUSD_ControllerFactory } from "./utils/Constants.js";
-import { livemodePegKeepers } from "./utils/pegkeeper/Pegkeeper.js";
-import { ABI_crvUSD_ControllerFactory } from "./utils/abis/ABI_crvUSD_ControllerFactory.js";
-import { launchCurveLendingMonitoring } from "./utils/Lending/LlamalendMain.js";
+import { getWeb3WsProvider } from './utils/helperFunctions/Web3.js';
+import { getCurrentBlockNumber, getPastEvents, } from './utils/web3Calls/generic.js';
+import { telegramBotMain } from './utils/telegram/TelegramBot.js';
+import { EventEmitter } from 'events';
+import { handleLiveEvents, manageMarket, watchingForNewMarketOpenings, } from './utils/Oragnizer.js';
+import { ADDRESS_crvUSD_ControllerFactory } from './utils/Constants.js';
+import { livemodePegKeepers } from './utils/pegkeeper/Pegkeeper.js';
+import { ABI_crvUSD_ControllerFactory } from './utils/abis/ABI_crvUSD_ControllerFactory.js';
+import { launchCurveLendingMonitoring } from './utils/Lending/LlamalendMain.js';
 console.clear();
 // ********************* classic crvUSD **************
 export const MIN_REPAYED_AMOUNT_WORTH_PRINTING = 100000;
@@ -17,13 +17,13 @@ export const MIN_HARDLIQ_AMOUNT_WORTH_PRINTING = 5000;
 // export const MIN_HARDLIQ_AMOUNT_WORTH_PRINTING = 0;
 // ********************* Lending *********************
 export const LENDING_MIN_LOAN_CHANGE_AMOUNT_WORTH_PRINTING = 1000;
-export const LENDING_MIN_LIQUIDATION_DISCOUNT_WORTH_PRINTING = 2;
+export const LENDING_MIN_LIQUIDATION_DISCOUNT_WORTH_PRINTING = 10;
 export const LENDING_MIN_HARDLIQ_AMOUNT_WORTH_PRINTING = 0;
 // export const LENDING_MIN_LOAN_CHANGE_AMOUNT_WORTH_PRINTING = 0;
 // export const LENDING_MIN_LIQUIDATION_DISCOUNT_WORTH_PRINTING = 0;
 // export const LENDING_MIN_HARDLIQ_AMOUNT_WORTH_PRINTING = 0;
 // ***************************************************
-const ENV = "prod";
+const ENV = 'prod';
 // const ENV = "test";
 const eventEmitter = new EventEmitter();
 async function launchClassicCrvUSDMonitoring() {
@@ -33,8 +33,9 @@ async function launchClassicCrvUSDMonitoring() {
     const PRESENT = await getCurrentBlockNumber();
     await livemodePegKeepers(PRESENT, eventEmitter);
     // await pegkeeperHisto(eventEmitter, 19046609, 19096615);
-    const ADDED_MARKET_EVENTS = await getPastEvents(crvUSD_ControllerFactory, "AddMarket", crvUSD_LAUNCH_BLOCK, PRESENT);
-    if (!(ADDED_MARKET_EVENTS instanceof Array))
+    const ADDED_MARKET_EVENTS = await getPastEvents(crvUSD_ControllerFactory, 'AddMarket', crvUSD_LAUNCH_BLOCK, PRESENT);
+    if (!(ADDED_MARKET_EVENTS instanceof
+        Array))
         return;
     for (const MARKET_CREATION of ADDED_MARKET_EVENTS) {
         await manageMarket(MARKET_CREATION, eventEmitter);
@@ -42,7 +43,7 @@ async function launchClassicCrvUSDMonitoring() {
     }
     await watchingForNewMarketOpenings(crvUSD_ControllerFactory, eventEmitter);
     await handleLiveEvents(eventEmitter);
-    console.log("crvUSD_Bot launched successfully.");
+    console.log('crvUSD_Bot launched successfully.');
 }
 async function main() {
     await telegramBotMain(ENV, eventEmitter);
